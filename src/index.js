afa30180e9284f37fdea202a691491c0516cffac
Jesús Fonseca Serrano <jesusfonseruma@gmail.com>
2025-06-30 17:35:25 +0200

    Primera llamada exitosa a la API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hola");
+import "./style.css"
+
+async function callDaInfo(){
+    const info = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/antequera?key=FYUUEU4LM4S82AZ5BT38GFSJC");
+    const processed = await info.json();
+    console.log(processed);
+}
+
+callDaInfo();
+
