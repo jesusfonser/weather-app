ba92008b594b5878c0f80404134f728cc303be0f
Jesús Fonseca Serrano <jesusfonseruma@gmail.com>
2025-06-30 19:17:02 +0200

    Controla llamadas a la API con el input

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import "./style.css"
 
-async function callDaInfo(){
-    const info = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/antequera?key=FYUUEU4LM4S82AZ5BT38GFSJC");
-    const processed = await info.json();
-    console.log(processed);
+const button = document.querySelector("button");
+const bigdiv = document.querySelector("#big-container");
+
+async function callDaInfo(city){
+        const info = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=FYUUEU4LM4S82AZ5BT38GFSJC`);
+        const processed = await info.json();
+        return processed;
 }
 
-callDaInfo();
+async function mostrarConsola(){
+    try{
+        const value = document.querySelector("input").value
+        const datos = await callDaInfo(value);
+        bigdiv.style.backgroundColor = "green";
+        console.log(datos);
+    }
+    catch (error){
+        console.log("Error acá" + error);
+        bigdiv.style.backgroundColor = "red";
+    }
+}
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    mostrarConsola()});
+
 
 
 /*
