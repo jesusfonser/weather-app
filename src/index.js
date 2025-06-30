acbad4266ab97d50787626710ac14d083d3ca2a2
Jesús Fonseca Serrano <jesusfonseruma@gmail.com>
2025-06-30 18:14:06 +0200

    Añade h1, h3 y div a la pagina

--- src/index.js
+++ src/index.js
@@ -8,3 +8,21 @@ async function callDaInfo(){
 
 callDaInfo();
 
+/*
+Pantalla de carga
+Input para meter una ciudad distinta
+Construye algo que recoja todos los dias con sus datos
+Frase que recoja prediccion general
+Por cada dia, construye una celda que muestre:
+    - Fecha
+    - Imagen
+    - Temperatura
+    - Temperatura en C
+    - Enlace a tiempo detallado para ese día:
+        + Tiempo por horas
+        + Tmax y Tmin, C y F
+        + Humedad?
+        + Precipitaciones
+
+*/
+
