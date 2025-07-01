34bb8a00a8bf7645bdf42ce0b59b6630e0c6e6de
Jesús Fonseca Serrano <jesusfonseruma@gmail.com>
2025-07-01 18:12:53 +0200

    Extrae información para cada celda a mostrar
    Puesto que se mostrarán iconos que reflejen el tiempo que hará, se han
    descargado iconos oficiales de la API VisualCrossing.

---
+++ src/data.js
@@ -0,0 +1,116 @@
+/*
+
+async function callDaInfo(city){
+        const info = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=FYUUEU4LM4S82AZ5BT38GFSJC`);
+        const processed = await info.json();
+        return processed;
+}
+
+async function mostrarConsola(){
+    try{
+        loaddiv.style.display = "block";
+        const value = document.querySelector("input").value
+        const datos = await callDaInfo(value);
+        bigdiv.style.backgroundColor = "green";
+        return datos;
+    } catch (error){
+        console.log("Error acá" + error);
+        bigdiv.style.backgroundColor = "red";
+    } finally {
+        loaddiv.style.display = "none";
+    }
+}
+*/
+
+/*
+Pantalla de carga
+Input para meter una ciudad distinta
+Construye algo que recoja todos los dias con sus datos
+Frase que recoja prediccion general
+Por cada dia, construye una celda que muestre:
+    - Fecha
+    - Imagen
+    - Temperatura
+    - Temperatura en C
+    - Enlace a tiempo detallado para ese día:
+        + Tiempo por horas
+        + Tmax y Tmin, C y F
+        + Humedad?
+        + Precipitaciones
+
+*/
+
+const bigdiv = document.querySelector("#big-container");
+const loaddiv = document.querySelector(".loader");
+let arrayDays = [];
+
+async function getData(city){
+    try{
+        const info = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=FYUUEU4LM4S82AZ5BT38GFSJC`);
+        const processed = await info.json();
+        //TODO: Meter función que modifique el DOM cuando se pille la info
+        return processed;
+    } catch (err){
+        //TODO: Meter función que modifique el DOM cuando haya error pillando info
+        console.log(err);    
+        return;
+        }
+}
+
+
+function buttonAPI(e){
+  e.preventDefault();
+  const cityInput = document.querySelector("input").value;
+  getData(cityInput).then(res => {
+    res.days.forEach(day => {
+        transformIntoDataCell(day)
+    })
+    console.log(arrayDays)}); //TODO: Función que procese la info de la API y de 15 dataCell
+}
+
+function transformIntoDataCell(day) {
+    let newCell = new dataCell (
+        day.tempmax,
+        day.tempmin,
+        day.temp,
+        day.precip,
+        day.datetime,
+        day.icon,
+        day.conditions,
+        day.hours,
+        day.description,
+        day.dew
+    )
+
+    arrayDays.push(newCell);
+}
+
+
+
+class dataCell {
+  constructor(
+    tMax,
+    tMin,
+    temp,
+    precip,
+    date,
+    icon,
+    weather,
+    hours,
+    fullWeather,
+    humedad,
+  ) {
+    this.tMax = tMax;
+    this.tMin = tMin;
+    this.temp = temp;
+    this.precip = precip;
+    this.date = date;
+    this.img = `./img/icons/${icon}.svg`;
+    this.weather = weather;
+    this.hours = hours;
+    this.fullWeather = fullWeather;
+    this.humedad = humedad;
+  }
+}
+
+ export { buttonAPI }
--- src/index.js
+++ src/index.js
@@ -1,52 +1,7 @@
 import "./style.css"
+import { buttonAPI } from "./data.js"
 
 const button = document.querySelector("button");
-const bigdiv = document.querySelector("#big-container");
-const loaddiv = document.querySelector(".loader");
-
-async function callDaInfo(city){
-        const info = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=FYUUEU4LM4S82AZ5BT38GFSJC`);
-        const processed = await info.json();
-        return processed;
-}
-
-async function mostrarConsola(){
-    try{
-        loaddiv.style.display = "block";
-        const value = document.querySelector("input").value
-        const datos = await callDaInfo(value);
-        bigdiv.style.backgroundColor = "green";
-        console.log(datos);
-    }
-    catch (error){
-        console.log("Error acá" + error);
-        bigdiv.style.backgroundColor = "red";
-    } finally {
-        loaddiv.style.display = "none";
-    }
-}
-
-button.addEventListener("click", (e) => {
-    e.preventDefault();
-    mostrarConsola()});
 
 
-
-/*
-Pantalla de carga
-Input para meter una ciudad distinta
-Construye algo que recoja todos los dias con sus datos
-Frase que recoja prediccion general
-Por cada dia, construye una celda que muestre:
-    - Fecha
-    - Imagen
-    - Temperatura
-    - Temperatura en C
-    - Enlace a tiempo detallado para ese día:
-        + Tiempo por horas
-        + Tmax y Tmin, C y F
-        + Humedad?
-        + Precipitaciones
-
-*/
-
+button.addEventListener("click", buttonAPI);
